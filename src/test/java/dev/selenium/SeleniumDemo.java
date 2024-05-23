7c28111f851975bf42be9e541c0e7cad8c09fd7e
goshogit <goshh93@gmail.com>
2024-05-23 12:19:46 +0300

    initial commit

--- src/test/java/dev/selenium/SeleniumDemo.java
+++ src/test/java/dev/selenium/SeleniumDemo.java
@@ -1,72 +1,30 @@
 package dev.selenium;
 
+import dev.MainDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class SeleniumDemo {
+public class SeleniumDemo extends MainDriver {
 
-WebDriver driver;
 
-@BeforeMethod
-public void before() {
-    driver = new ChromeDriver();
 
-    driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
-}
+
 
     @Test
-    public void testOpenBrowser(){
+    public void testOpenBrowser() {
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        WebElement textinput = driver.findElement(By.cssSelector("my password"));
+        textinput.click();
 
-        driver.getTitle();
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-
-        WebElement textBox = driver.findElement(By. id("my-text-id"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
-
-        driver.navigate().refresh();
-        textBox.sendKeys("Selenium");
-        submitButton.click();
-
-
-        WebElement message = driver.findElement(By.id("message"));
-        message.getText();
-
+        WebElement textinput = driver.findElement(By.);
+        textinput.click();
     }
 
-    public void testOpenBrowser2(){
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
-        driver.getTitle();
-
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-
-        WebElement textBox = driver.findElement(By. id("my-text-id"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
-
-        driver.navigate().refresh();
-        textBox.sendKeys("Selenium");
-        submitButton.click();
-
-
-        WebElement message = driver.findElement(By.id("message"));
-        message.getText();
-
-    }
-
-    @AfterMethod
-    public void tearDown(){
-        driver.quit();
-    }
 }
